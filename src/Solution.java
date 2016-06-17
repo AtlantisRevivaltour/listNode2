509fea6e30ec55b957f4900cdf59bfa164158347
AtlantisRevivaltour <mail.yangchun@163.com>
2016-06-17 12:44:54 +0800

    leetcoder

---
+++ src/Solution.java
@@ -0,0 +1,18 @@
+import res.ListNode;
+
+/**
+ * Created by Syusuke on 2016/4/24 0024.
+ */
+public class Solution {
+
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+
+
+        return  null;
+    }
+
+    public static void main(String[] args ){
+
+        System.out.println("xxxxx");
+    }
+}
---
+++ src/res/ListNode.java
@@ -0,0 +1,19 @@
+package res;
+
+/**
+ * Created by Syusuke on 2016/4/24 0024.
+ */
+public class ListNode {
+    /**
+     * Definition for singly-linked list.
+     */
+          int val;
+          ListNode next;
+
+          ListNode(int x) {
+              val = x;
+          }
+
+
+
+}
